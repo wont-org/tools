380d65ad7e904b61adb885703bb1aaa0873b1393
liukun <919590347@qq.com>
2021-07-08 21:07:44 +0800

    fix: 先生成入口文件再进行打包

--- compiler/index.ts
+++ compiler/index.ts
@@ -6,16 +6,22 @@ import { clean } from './cleanTask'
 import { rollupCompileAll } from './scriptsRollup'
 
 const compileLess = parallel(less2css, copyLess)
-const compileScripts = parallel(genEntry, compileESM, compileCJS, genTypes)
+const compileScripts = parallel(compileESM, compileCJS, genTypes)
 
-const compileUiComponents = series(clean, parallel(compileLess, compileScripts))
+const compileUiComponents = series(
+    clean,
+    genEntry,
+    parallel(compileLess, compileScripts),
+)
 const compileUiComponentsWithRollup = series(
     clean,
+    genEntry,
     parallel(compileLess, rollupCompileAll)
 )
 const compileUtilsWithRollup = series(
     clean,
-    parallel(genUtilsEntry, rollupCompileAll)
+    genUtilsEntry,
+    rollupCompileAll,
 )
 
 task('compileUiComponents', compileUiComponents)
