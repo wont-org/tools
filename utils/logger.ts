6e92f350f30122aab5afba44ed9ef35a1161effa
bantang <919590347@qq.com>
2021-07-14 10:11:43 +0800

    chore: log增加ts类型

--- utils/logger.ts
+++ utils/logger.ts
@@ -1,13 +1,21 @@
 import chalk from 'chalk'
 
+interface LogParams {
+    logType?: 'Info:' | 'Success:' | 'Error:' | string
+    text: string
+}
+
 export const log = {
-    info: ({ logType = 'Info:', text }) => {
+    info: (params: LogParams) => {
+        const { logType = 'Info:', text } = params
         console.log(`\n${chalk.blue.bold(logType)} ${chalk.gray(text)}`)
     },
-    ok: ({ logType = 'Success:', text }) => {
+    ok: (params: LogParams) => {
+        const { logType = 'Success:', text } = params
         console.log(`\n${chalk.green.bold(logType)} ${chalk.cyan(text)}`)
     },
-    error: ({ logType = 'Error:', text }) => {
+    error: (params: LogParams) => {
+        const { logType = 'Error:', text } = params
         console.log(`\n${chalk.red.bold(logType)} ${chalk.red(text)}`)
     },
 }
